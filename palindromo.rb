255df501e7e978f41855ccda1cd82206b924dfa1
Daniel Castro <79341302+DanielCastro92@users.noreply.github.com>
2021-03-17 18:05:03 -0300

    Add files via upload

---
+++ palindromo.rb
@@ -0,0 +1,28 @@
+puts "-" * 10 + " Exercício 01 - Palíndromo " + "-"*10
+print "Digite uma palavra: "
+palavra = gets.chomp
+
+tabela = palavra.chars
+tamanho = tabela.length
+contador = 0
+validador = nil
+
+def comparar?(a, b)
+    a == b
+end
+
+while contador >= 0 && contador <= tamanho - 1
+    if comparar?(tabela[contador], tabela[-1 - contador]) == true
+        validador = 1
+        contador = contador + 1
+    else
+        validador = 2
+        contador = contador + 1
+    end
+end
+
+if validador == 1 
+    puts "A palavra #{palavra} é palíndroma"
+else
+    puts "A palavra #{palavra} não é palíndroma"
+end
